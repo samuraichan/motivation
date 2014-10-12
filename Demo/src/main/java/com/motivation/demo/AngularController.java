698c332db708a125fe6e01fb3234e14e66a3b69f
rchiari <rchiari@gmail.com>
2014-10-12 16:32:08 -0600

    initial checkin, angluar demo wrapped in spring mvc container

---
+++ Demo/src/main/java/com/motivation/demo/AngularController.java
@@ -0,0 +1,24 @@
+package com.motivation.demo;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class AngularController {
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {	
+		return "/pages/index.html";
+	}
+	
+}
---
+++ Demo/src/main/webapp/js/MainController.js
@@ -0,0 +1,23 @@
+function MainController($scope, photoSearchService) {
+
+  'use strict';
+  
+  $scope.photos = [];
+  
+  $scope.thumbSize = 'small';
+  
+  $scope.setThumbSize = function(size) {
+    $scope.thumbSize = size;
+  };
+  
+  $scope.submitSearch = function() {
+
+	  $scope.photos = [];
+	
+	  var keyword = $scope.searchKeyword;
+	  photoSearchService.findPhotos(keyword, function(photos) {
+	    $scope.photos = photos;
+	  });
+	};
+
+}
